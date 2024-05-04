8cf55a2969a90c89812b47d1fbbe85cd67e45d76
Asyurazhr <Asyuradjola2004@gmail.com>
2024-05-04 22:52:54 +0700

    create case 3

--- 0231_Doublelinkedlist/0231_Doublelinkedlist.cpp
+++ 0231_Doublelinkedlist/0231_Doublelinkedlist.cpp
@@ -215,6 +215,9 @@ int main()
             case '2':
                 deleteNode();
                 break;
-            
+            case '3':
+                traverse();
+                break;
+
 }
 
